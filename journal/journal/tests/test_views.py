27ae23f22dbbb18b0553692df041586a8da8c711
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-19 16:09:43 -0700

    home and detail tests

--- journal/journal/tests/test_views.py
+++ journal/journal/tests/test_views.py
@@ -1,6 +1,32 @@
 from journal.models import Entry, DBSession
+from journal.views import my_view, compose, entry_detail
 from pyramid.testing import DummyRequest
 from pyramid.response import Response
+
+
+def test_my_view(dbtransaction, new_entry):
+    """Test my_view retunrs a lists entries in it's response object."""
+    req = DummyRequest()
+    resp = my_view(req)
+    entries_dict = resp['entries']
+    assert entries_dict[0] == new_entry
+
+
+def test_entry_detail_view(dbtransaction, new_entry):
+    """Test entry_detail returns a list of entries in it's reposnse object."""
+    req = DummyRequest()
+    req.matchdict = {'entry.id': new_entry.id}
+    resp_dict = entry_detail(req)
+    assert resp_dict['entry'] == new_entry
+
+
+# def test_entry_detail_view(dbtransaction, new_entry):
+#     """Test that list_view returns a Query of Entries."""
+#     test_request = DummyRequest()
+#     test_request.matchdict = {'detail_id': new_entry.id}
+#
+#     response_dict = detail_view(test_request)
+#     assert response_dict['entry'] == new_entry
 
 
 # class test_NewEntry(Form):
